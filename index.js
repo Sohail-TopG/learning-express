cf926f497a9e8153affb455bb6f23c011c3420fe
Sohail-Fladdra <sohail.ansari@fladdra.com>
2023-09-07 10:05:03 +0530

    made hello word

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('<h1>Hello World!</h1>')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
