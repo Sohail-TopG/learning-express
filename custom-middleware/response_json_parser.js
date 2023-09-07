4c775266d586fe6089c9dfd088ed0945bf29bf23
Sohail-Fladdra <sohail.ansari@fladdra.com>
2023-09-07 12:57:28 +0530

    made response jason parser middleware

---
+++ custom-middleware/response_json_parser.js
@@ -0,0 +1,9 @@
+(req, res, next) => {
+    var send = res.send;
+    res.send = function (string) {
+      var body = string instanceof Buffer ? string.toString() : string;
+      console.log(body);
+      send.call(this, body);
+    };
+    next();
+  }
--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.json()); // <==== parse request body as JSON
+app.use((req, res, next) => {
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
+  next();
+});
+
+app.get("/", (req, res) => {
+  //   res.json({ msg: "Got a GET request in JSOn" });
+  res.send("Got a GET request");
+});
+app.post("/", (req, res) => {
+  res.send("Got a POST request");
+});
+app.put("/user", (req, res) => {
+  res.send("Got a PUT request at /user");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Got a DELETE request at /user");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
